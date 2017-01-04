22d0e76df55456f81c62e963fd601bee30c82ba9
Karla Hugo <karlabrandon817@gmail.com>
2017-01-04 10:57:35 -0600

    objects on DOM

--- scripts/ngLaunchpad.js
+++ scripts/ngLaunchpad.js
@@ -3,9 +3,12 @@ var myApp = angular.module( 'myApp', []);
 myApp.controller('LaunchpadController', ['$scope', function($scope){
   $scope.myMovies = [];
   $scope.getMovieName = function(){
-    console.log('in getMovieName()');
-    console.log('movieName:', $scope.movieName);
-    $scope.myMovies.push($scope.movieName);
+    var favMovies = {
+      movie: $scope.movieName,
+      year: $scope.movieYear
+    };
+    console.log('movieName:', $scope.movieName + ' ' + $scope.movieYear);
+    $scope.myMovies.push(favMovies);
     console.log('movie array:', $scope.myMovies);
   };//end getMovieName
 }]);
